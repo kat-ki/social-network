a9ff8402c71ad1cad99e36940f9918f8cdfae007
kat-ki <glraskat@gmail.com>
2023-03-29 13:43:23 +0200

    29.03

---
+++ src/components/Profile.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div>
+        <div>
+        <img
+    src='https://wallup.net/wp-content/uploads/2019/05/10/1025950-featured-fresh-and-beautiful-blue-sea-waves.jpg'
+    alt='sea photo'/>
+        </div>
+    <div>
+        <img src='https://www.logolynx.com/images/logolynx/0c/0c79eccd47ca898469ee2e9b12bbf907.jpeg'
+             alt='profile-photo'/>
+        profile description
+    </div>
+    <div>my posts
+        <div>new post box with send button </div>
+    </div>
+    <div> old posts
+        <div>post 1</div>
+        <div>post 2</div>
+        <div>post 3</div>
+    </div>
+</div>
+    )
+}
+
+export default Profile;
