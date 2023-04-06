ef4e2483f37815c8f2501adc0d81ceb81d379e51
kat-ki <glraskat@gmail.com>
2023-04-06 23:21:58 +0200

    05.04

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -2,20 +2,14 @@ import React from 'react';
 import styleCl from './Profile.module.css'
 import MyPosts from "./MyPosts/MyPosts";
 import pic2 from "./pic2.jpg"
+import ProfileInfo from "./ProfileInfo/PorfileInfo";
 
 /* export type ProfilePropsType = {} */
 
 const Profile = () => {
     return (
         <div className={styleCl.item}>
-            <div>
-                <img
-                    src={pic2}
-                    alt=''/>
-            </div>
-            <div>
-                <div> profile description</div>
-            </div>
+            <ProfileInfo />
             <MyPosts />
         </div>
     )
